0454158378dd44c4718bcb17e3622556216384e6
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2025-09-29 14:04:30 -0300

    --no-edit

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -21,7 +21,7 @@
 					>Login</a
 				>
 				<a
-					href="#"
+					href="/"
 					class="mr-2 rounded-lg bg-emerald-700 px-4 py-2 text-sm font-medium text-white hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 focus:outline-none lg:px-5 lg:py-2.5 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
 					>Cadastre-se</a
 				>
